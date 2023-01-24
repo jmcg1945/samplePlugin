d644e936c36f43528a6f258486fb3f7a994d5608
joseph.mcgraw <joseph.mcgraw@ip-192-168-1-251.ec2.internal>
2023-01-24 09:46:50 -0500

    Unzip File and Read Xml

--- Sample Project/src/com/icfi/logconfig/LogConfiguration.java
+++ Sample Project/src/com/icfi/logconfig/LogConfiguration.java
@@ -4,74 +4,63 @@ import com.appiancorp.suiteapi.expression.annotations.Category;
 import com.appiancorp.suiteapi.expression.annotations.Function;
 import com.appiancorp.suiteapi.expression.annotations.Parameter;
 import com.appiancorp.suiteapi.content.*;
+import com.appiancorp.suiteapi.content.exceptions.InvalidContentException;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.util.Scanner;
-
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 
 @Category("logConfigCategory")
 public class LogConfiguration {
 
 	  @Function
-	  public static  String runFunction(ContentService cs, @Parameter long arg) throws Exception{
-	FileInputStream fis = null;
-	WordExtractor w = null;
-	try {
-		  String filename = cs.getInternalFilename(arg);
-		  //fs = new FileSystem(filename);
-		 fis = new FileInputStream(filename);
-
-		  
-
-		  
-		  
-		  
-		  
-		  //POIDocument file   = new POIDocument(OPCPackage.open(fis));
-		  //WordExtractor ext = new WordExtractor(fis);
-		  
-		
-	      //Creating a Scanner object	  
-	      //Scanner sc = new Scanner(fis);     
-	      //StringBuffer sb = new StringBuffer();
-	      //while(sc.hasNext()){
-	      //   sb.append(sc.nextLine());
-	      //}
-	      //return(sb.toString());
-	      // scanner ends
-	      
-	      
-		  	  
-		  //output
-		  		//return new String(ext);
-		  		//System.out.println(ext.getText());
-		  
-		  //gets bytes
-		  		w = new WordExtractor(fis);
-		  		byte[] b = new byte[1024];
-		  		fis.read(b);
-		  		return new String(b);
-		  //bytes
+	  public static  String runFunction(ContentService cs, @Parameter Long arg) throws IOException, InvalidContentException {
+		  	String filename = cs.getInternalFilename(arg);
+	        byte[] buffer = new byte[1024];
+	        FileInputStream fis = new FileInputStream(filename);
+	        ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+	        ZipEntry ze;
+	        String xmlContent = "";
+	        
+	        while ((ze = zis.getNextEntry()) != null) {
+	            File newFile = new File(filename.substring(0, filename.lastIndexOf(".")), ze.getName());
+	            if (ze.isDirectory()) {
+	                newFile.mkdirs();
+	                continue;
+	            } else {
+	                newFile.getParentFile().mkdirs();
+	            }
+	            FileOutputStream fos = new FileOutputStream(newFile);
+	            int len;
+	            while ((len = zis.read(buffer)) > 0) {
+	                fos.write(buffer, 0, len);
+	            }
+	            fos.close();
+	            if(ze.getName().equals(filename)){
+	              InputStream is = new FileInputStream(newFile);
+	              ByteArrayOutputStream result = new ByteArrayOutputStream();
+	              byte[] buffer1 = new byte[1024];
+	              int length;
+	              while ((length = is.read(buffer1)) != -1) {
+	                  result.write(buffer1, 0, length);
+	              }
+	              xmlContent = result.toString("UTF-8");
+	              is.close();
+	            }
+	        }
+	        zis.close();
+	        fis.close();
+	        return xmlContent;
+	    }
 	}
-			  finally {
-				  //we.close();
-				  fis.close();
-				  w.close();
-		  }
-			 
-			  
-		  }
-}
+
 
 
 
