cddf36270cedb73fc17ad82442a32f509516f10c
joseph.mcgraw <joe.f.mcgraw@gmail.com>
2023-02-03 08:44:20 -0500

    If loop error: otherwise all working

--- Sample Project/src/com/icfi/logconfig/LogConfiguration.java
+++ Sample Project/src/com/icfi/logconfig/LogConfiguration.java
@@ -1,25 +1,39 @@
 package com.icfi.logconfig;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.commons.compress.utils.IOUtils;
-import org.w3c.dom.NodeList;
 
+
+
+
+//import org.w3c.dom.*;
+import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
 
 import com.appiancorp.suiteapi.content.ContentConstants;
 import com.appiancorp.suiteapi.content.ContentService;
 import com.appiancorp.suiteapi.expression.annotations.Category;
 import com.appiancorp.suiteapi.expression.annotations.Function;
 import com.appiancorp.suiteapi.expression.annotations.Parameter;
-import com.appiancorp.suiteapi.knowledge.Document;
+//import com.appiancorp.suiteapi.knowledge.Document;
+
 
 
 
@@ -27,7 +41,8 @@ import com.appiancorp.suiteapi.knowledge.Document;
 
 @Category("logConfigCategory")
 public class LogConfiguration {
-
+	
+	
 	private static final Logger LOGGER = Logger.getLogger(LogConfiguration.class.getName());
 	
 	  @Function
@@ -36,8 +51,7 @@ public class LogConfiguration {
 		  int len;
 		  
 		  
-			  byte[] buffer = new byte[1024];
-			  StringBuffer sb = new StringBuffer();
+
 			  String filename = cs.getInternalFilename(arg);
 			  
 			  File inputDirectory = new File(filename);
@@ -52,49 +66,42 @@ public class LogConfiguration {
 				  
 				  if(zipEntry.getName().equalsIgnoreCase("Word/document.xml")) {
 					  LOGGER.info("Found word/document.xml");
-					  Document xmlDoc = new Document(1225L, "myXMLDocument", "xml");
+					  com.appiancorp.suiteapi.knowledge.Document xmlDoc = new com.appiancorp.suiteapi.knowledge.Document(1225L, "myXMLDocument", "xml");
 					  Long docId = cs.create(xmlDoc, ContentConstants.UNIQUE_NONE);
 					  String fosFileName = cs.getInternalFilename(docId);					 
 					  LOGGER.info("Filename is " + fosFileName);
 					  FileOutputStream fos = new FileOutputStream(fosFileName);
 					  IOUtils.copy(zipInputStream, fos);
 					  fos.close();
-					  File readXML = new File(cs.getInternalFilename(docId));
-					  DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-					  DocumentBuilder builder = factory.newDocumentBuilder();	
 					  
-					// Load the input XML document, parse it and return an instance of the
-				        // Document class.
-				        //org.w3c.dom.Document document = builder.parse(readXML);
-				        //document.getDocumentElement().normalize();
-				       // NodeList nodeList = document.getElementsByTagName("w:t");
-				        
-				       // for (int itr = 0; itr < nodeList.getLength(); itr++) {
-				       //     LOGGER.info(nodeList.item(itr).getNodeValue());				            
-				       //     if (nodeList.item(itr).hasChildNodes()) {
-				       //       NodeList nodeList2 = nodeList.item(itr).getChildNodes();
-				       //       for (int itr2 = 0; itr2 < nodeList2.getLength(); itr2++) {
-				       //         LOGGER.info(" Child node: " + nodeList2.item(itr2).getNodeName() + " value: " + nodeList2.item(itr2).getNodeValue());
-				        
-				 // }
-			  	//}
-			 // }
-		 
-				     //   LOGGER.info("Length of nodes was: " + nodeList.getLength());
-	
-	    }
-	  zipInputStream.closeEntry();
-	}
-			return filename;
-			  
+					  File readXML = new File(cs.getInternalFilename(docId));
+					  BufferedReader reader = new BufferedReader(new FileReader(readXML));
+
+					  String line = reader.readLine();
+					  StringBuilder sb = new StringBuilder();
+					 
+					  while (line != null) {
+						  LOGGER.info("Start of while loop");
+					    if (line.contains("<w:t>")) {
+					      int startIndex = line.indexOf("<w:t>") + "<w:t>".length();
+					      int endIndex = line.indexOf("</w:t>");
+					      String value = line.substring(startIndex, endIndex);
+					      LOGGER.info("Value: " + value);
+					      LOGGER.info("Line value is:" + line);
+					    }
+					    line = reader.readLine();
+					    LOGGER.info("End of Loop");
+					  }
+					 
+					  reader.close();
+					  
 		  } 
 		  
 	  
 	  }
-
+			return filename;
 			
-
-//
-
+	  }
+}
 
 
